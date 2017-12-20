a98c52ecc2e4c8077a01ede35af1db681c52592a
resurtm <resurtm@gmail.com>
2017-12-20 18:49:54 +0600

    problem 20, part 2

--- problem20/main.go
+++ problem20/main.go
@@ -20,14 +20,17 @@ type vector struct {
 
 type particle struct {
 	p, v, a vector
+	dead    bool
 }
 
 var testCasesPartOne = []testCase{
 	{input: "input0.txt", result: 0},
-	{input: "input1.txt", result: -1},
+	{input: "input1.txt", result: 91},
 }
 
 var testCasesPartTwo = []testCase{
+	{input: "input2.txt", result: 1},
+	{input: "input1.txt", result: 567},
 }
 
 func RunPartOne() {
@@ -55,21 +58,9 @@ func RunPartTwo() {
 }
 
 func (tc *testCase) solvePartOne() int {
-	inputData := readInput(tc.input)
-	particles := []particle{}
-	for _, inputLine := range inputData {
-		part := particle{}
-		fmt.Sscanf(
-			inputLine,
-			"p=<%d,%d,%d>, v=<%d,%d,%d>, a=<%d,%d,%d>",
-			&part.p.x, &part.p.y, &part.p.z,
-			&part.v.x, &part.v.y, &part.v.z,
-			&part.a.x, &part.a.y, &part.a.z,
-		)
-		particles = append(particles, part)
-	}
+	particles := parseInput(tc.input)
 
-	for i := 0; i < 100000; i++ {
+	for iters := 0; iters < 500; iters++ {
 		for i, part := range particles {
 			part.v.x += part.a.x
 			part.v.y += part.a.y
@@ -97,7 +88,63 @@ func (tc *testCase) solvePartOne() int {
 }
 
 func (tc *testCase) solvePartTwo() int {
-	return 0
+	particles := parseInput(tc.input)
+
+	for i := 0; i < 500; i++ {
+		for i, parti := range particles {
+			if parti.dead {
+				continue
+			}
+			for j, partj := range particles {
+				if partj.dead || i == j {
+					continue
+				}
+				if parti.p.x == partj.p.x && parti.p.y == partj.p.y && parti.p.z == partj.p.z {
+					particles[i].dead = true
+					particles[j].dead = true
+				}
+			}
+		}
+
+		for i, part := range particles {
+			if part.dead {
+				continue
+			}
+			part.v.x += part.a.x
+			part.v.y += part.a.y
+			part.v.z += part.a.z
+			part.p.x += part.v.x
+			part.p.y += part.v.y
+			part.p.z += part.v.z
+			particles[i] = part
+		}
+	}
+
+	alive := 0
+	for _, part := range particles {
+		if part.dead {
+			continue
+		}
+		alive++
+	}
+	return alive
+}
+
+func parseInput(inputFile string) []particle {
+	inputData := readInput(inputFile)
+	particles := []particle{}
+	for _, inputLine := range inputData {
+		part := particle{}
+		fmt.Sscanf(
+			inputLine,
+			"p=<%d,%d,%d>, v=<%d,%d,%d>, a=<%d,%d,%d>",
+			&part.p.x, &part.p.y, &part.p.z,
+			&part.v.x, &part.v.y, &part.v.z,
+			&part.a.x, &part.a.y, &part.a.z,
+		)
+		particles = append(particles, part)
+	}
+	return particles
 }
 
 func readInput(fn string) []string {
